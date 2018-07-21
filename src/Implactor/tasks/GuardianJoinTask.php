f23a10300dec53bc2f258a7ad10e932f600589b3
Zade <41181506+Zadezter@users.noreply.github.com>
2018-07-21 14:12:45 +0000

    Update GuardianJoinTask.php

--- src/Implactor/tasks/GuardianJoinTask.php
+++ src/Implactor/tasks/GuardianJoinTask.php
@@ -1,4 +1,26 @@
 <?php
+/**
+*
+* 
+*  _____                 _            _             
+* |_   _|               | |          | |            
+*   | |  _ __ ___  _ __ | | __ _  ___| |_ ___  _ __ 
+*   | | | '_ ` _ \| '_ \| |/ _` |/ __| __/ _ \| '__|
+*  _| |_| | | | | | |_) | | (_| | (__| || (_) | |   
+* |_____|_| |_| |_| .__/|_|\__,_|\___|\__\___/|_|   
+*                 | |                               
+*                 |_|                               
+*
+* Implactor (c) 2018
+* This plugin is licensed under GNU General Public License v3.0!
+* It is free to use, copyleft license for software and other 
+* kinds of works.
+* ------===------
+* > Author: Zadezter
+* > Team: ImpladeDeveloped
+*
+*
+**/
 declare(strict_types=1);
 namespace Implactor\tasks;
 
@@ -18,17 +40,16 @@ class GuardianJoinTask extends Task {
 	private $player;
 	private $plugin;
 
-	public function __construct(Main $plugin, Player $player){
+	public function __construct(Implade $plugin, Player $player){
                 $this->plugin = $plugin;
                 $this->player = $player;
 	}
 	
 	public function onRun(int $currentTick): void{
-		$player = $this->player;
 		$pk = new LevelEventPacket();
 		$pk->evid = LevelEventPacket::EVENT_GUARDIAN_CURSE;
 		$pk->data = 0;
-		$pk->position = $player->asVector3();
-		$player->dataPacket($pk);
+		$pk->position = $this->player->asVector3();
+		$this->player->dataPacket($pk);
 	}
 }
